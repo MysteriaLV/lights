885fefdae0f811bba573ce5001864477313fa22f
Pavel Veretennikov <vermut@kid.lv>
2017-06-27 23:03:55 +0300

    rebuilt for ModbusSerial

--- src/lights_modbus.cpp
+++ src/lights_modbus.cpp
@@ -1,5 +1,10 @@
-#include <SimpleModbusSlave.h>
 #include "Atm_lights.h"
+#define USE_HOLDING_REGISTERS_ONLY
+#include <Modbus.h>
+
+extern void modbus_setup();
+extern void modbus_loop();
+extern void modbus_set(word event, word value);
 
 //////////////// registers of LIGHTS ///////////////////
 enum {
@@ -8,80 +13,116 @@ enum {
 
 	// Any registered events, denoted by 'triggered_by_register' in rs485_node of Lua script, 1 and up
 
-	TOTAL_ERRORS,     // leave this one, error counter
-	TOTAL_REGS_SIZE   // INTERNAL: total number of registers for function 3 and 16 share the same register array
+  TOTAL_ERRORS     // leave this one, error counter
 };
 
-unsigned int holdingRegs[TOTAL_REGS_SIZE]; // function 3 and 16 register array
-////////////////////////////////////////////////////////////
+#ifdef USE_SOFTWARE_SERIAL
+    #include <ModbusSerial.h>
+    ModbusSerial mb;
+
+    #define SSerialTxControl 6   //RS485 Direction control
+    #define SSerialRX        8  //Serial Receive pin
+    #define SSerialTX        9  //Serial Transmit pin
+    SoftwareSerial RS485Serial(SSerialRX, SSerialTX); // RX, TX
+#endif
+
+#ifdef USE_ALT_SOFT_SERIAL
+	#include <ModbusSerial.h>
+    ModbusSerial mb;
+
+    #define SSerialTxControl 6   //RS485 Direction control
+    #define SSerialRX        8  //Serial Receive pin
+    #define SSerialTX        9  //Serial Transmit pin
+    AltSoftSerial RS485Serial(SSerialRX, SSerialTX); // RX, TX
+#endif
+
+#ifdef USE_SERIAL1
+    #include <ModbusSerial.h>
+    ModbusSerial mb;
+
+    #define SSerialRX        19  //Serial3 Receive pin (just a reference, can't be changed)
+    #define SSerialTX        18  //Serial3 Transmit pin (just a reference, can't be changed)
+    #define SSerialTxControl 20   //RS485 Direction control
+
+    #define RS485Serial Serial1
+#endif
+
+#ifdef USE_ESP8266_TCP
+    #include <ESP8266WiFi.h>
+    #include <ModbusIP_ESP8266.h>
+    ModbusIP mb;
+#endif
 
 // Action handler. Add all your actions mapped by action_id in rs485_node of Lua script
 void process_actions() {
-	if (holdingRegs[ACTIONS] == 0)
+  if (mb.Hreg(ACTIONS) == 0)
 		return;
 
-	switch (holdingRegs[ACTIONS]) {
+  switch (mb.Hreg(ACTIONS)) {
 		case 1 : // Put here code for Go_dim
 			Serial.println("[Go_dim] action fired");
-			digitalWrite(LED_BUILTIN, LOW);
-			atm_lights.off();
+      digitalWrite(LED_BUILTIN, HIGH);
 			break;
 		case 2 : // Put here code for Go_normal
 			Serial.println("[Go_normal] action fired");
-			digitalWrite(LED_BUILTIN, HIGH);
-			atm_lights.full();
+      digitalWrite(LED_BUILTIN, LOW);
 			break;
 		case 3 : // Put here code for Go_alarms
 			Serial.println("[Go_alarms] action fired");
-			digitalWrite(LED_BUILTIN, HIGH);
-			atm_lights.alarm();
+      digitalWrite(LED_BUILTIN, LOW);
 			break;
 		case 4 : // Put here code for Go_off
 			Serial.println("[Go_off] action fired");
-			atm_lights.off();
+      digitalWrite(LED_BUILTIN, LOW);
 			break;
 		default:break;
 	}
 
 	// Signal that action was processed
-	holdingRegs[ACTIONS] = 0;
+  mb.Hreg(ACTIONS, 0);
 }
 
-// Just debug functions for easy testing. Won't be used probably
-/* Holds current button state in register */
-void buttonStatus(int reg, uint8_t pin) { // LOOP
-	holdingRegs[reg] = digitalRead(pin) ? 1 : 0;
+void modbus_set(word event, word value) {
+	mb.Hreg(event, value);
 }
-
-void buttonStatus_setup(int reg, uint8_t pin) { // SETUP
-	pinMode(pin, INPUT_PULLUP);
-}
-/////////////////////////////////////////////////////////////////
 
 void modbus_setup() {
-	Serial.println("Serial ModBus Slave LIGHTS:1 for lua/Aliens.lua");
+	Serial.println("ModBus Slave LIGHTS:1 for lua/Aliens.lua");
 
-	/* parameters(long baudrate,
-				  unsigned char ID,
-				  unsigned char transmit enable pin,
-				  unsigned int holding registers size)
-	*/
+#ifndef USE_ESP8266_TCP
+	mb.config(&RS485Serial, 57600, SSerialTxControl);
+	mb.setSlaveId(1);
+#endif
 
-	modbus_configure(57600, 1, SSerialTxControl, TOTAL_REGS_SIZE);
-	holdingRegs[ACTIONS] = 0;
-	// Sample calls
-	pinMode(LED_BUILTIN, OUTPUT);
-	// buttonStatus_setup(, <buttonPin>);
+
+#ifdef USE_ESP8266_TCP
+	mb.config("Aliens", "123123");
+  WiFi.config(IPAddress(1), IPAddress(), IPAddress(), IPAddress(), IPAddress());
+
+  Serial.print("Connecting to Aliens ");
+  while (WiFi.status() != WL_CONNECTED) {
+	delay(500);
+	Serial.print(".");
+  }
+
+  Serial.println(" CONNECTED!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Netmask: ");
+  Serial.println(WiFi.subnetMask());
+
+  Serial.print("Gateway: ");
+  Serial.println(WiFi.gatewayIP());
+#endif
+
+	mb.addHreg(ACTIONS, 0);
+
+	pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output (D4)
 }
 
 
 void modbus_loop() {
-	holdingRegs[TOTAL_ERRORS] = modbus_update(holdingRegs);
+	mb.task();              // not implemented yet: mb.Hreg(TOTAL_ERRORS, mb.task());
 	process_actions();
-
-	// Notify main console of local events
-
-
-	// Sample calls
-	// buttonStatus(, <buttonPin>);
 }
