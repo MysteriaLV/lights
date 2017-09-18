21d83069da017f71fc1f371c289f5762d66621e9
Pavel Veretennikov <vermut@kid.lv>
2017-09-18 21:14:49 +0300

    new rate

--- src/lights_modbus.cpp
+++ src/lights_modbus.cpp
@@ -113,7 +113,7 @@ void modbus_setup() {
 #endif
 
 #ifndef USE_ESP8266_TCP
-	mb.config(&RS485Serial, 57600, SSerialTxControl);
+	mb.config(&RS485Serial, 31250, SSerialTxControl);
 	mb.setSlaveId(1);
 #else
 	mb.config("Aliens Room", "123123123");
