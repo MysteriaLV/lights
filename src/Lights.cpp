18920bf626fbecf3d9793c07ec82c6e329ecdfef
Pavels Veretennikovs <pavels.veretennikovs@riskfocus.com>
2019-08-04 16:47:04 +0300

    -> uno
    relay blocks

--- src/Lights.cpp
+++ src/Lights.cpp
@@ -5,6 +5,8 @@ extern void modbus_setup();
 extern void modbus_loop();
 extern void modbus_set(word event, word value);
 
+Atm_led exit_door, xray_screen;
+
 /*
  * http://www.analog.com/media/en/technical-documentation/data-sheets/ADuM1200_1201.pdf
  * http://www.aimtec.com/site/Aimtec/files/Datasheet/HighResolution/AM1S-Z.pdf
@@ -22,7 +24,13 @@ void setup() {
 	modbus_setup();
 	atm_lights.begin();
 
+	exit_door.begin(10, true);
+    xray_screen.begin(11, true);
+
 #ifdef MY_TEST_MODE
+    exit_door.blink(5000, 5000).start();
+    xray_screen.blink(15000, 5000).start();
+
 	test_mode_timer1.begin(1000, 999)
 			.onTimer([](int idx, int v, int up) {
 				switch (v % 3) {
